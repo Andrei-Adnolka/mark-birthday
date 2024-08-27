31403efa0493783b6d8740d91f327d406e7683ba
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-08-27 23:07:58 +0300

    updated

--- src/components/gifts/index.tsx
+++ src/components/gifts/index.tsx
@@ -23,7 +23,7 @@ const CONFIG = [
     title: `<s>${ICON} Баскетбольное кольцо</s>`,
   },
   {
-    title: `${ICON} Грузовик автовоз с машинкой`,
+    title: `<s>${ICON} Грузовик автовоз с машинкой</s>`,
     link: "https://global.wildberries.ru/product/331885-gruzovik-avtovoz-s-mashinkoj-igrushechnyj-transport-41131794?option=82793943",
   },
   {
@@ -33,6 +33,34 @@ const CONFIG = [
   {
     title: `${ICON} Кулер для воды детский`,
     link: "https://global.wildberries.ru/product/331866-kuler-dlya-vody-detskij-interaktivnaya-igrushka-177850419?option=294457917",
+  },
+  {
+    title: `${ICON} Генератор пены Краб`,
+    link: "https://www.wildberries.by/product/generator-peny-krab-230150286?option=363510361",
+  },
+  {
+    title: `${ICON} Игрушки для ванной на присосках`,
+    link: "https://www.wildberries.by/product/igrushki-dlya-vannoj-na-prisoskakh-nabor-dlya-kupaniya-malyshej-160480520?option=266778410",
+  },
+  {
+    title: `${ICON} Набор тесто для лепки`,
+    link: "https://www.wildberries.by/product/nabor-testo-dlya-lepki-202998294?option=327171412",
+  },
+  {
+    title: `${ICON} Набор книжек с наклейками детскими`,
+    link: "https://www.wildberries.by/product/nabor-knizhek-s-naklejkami-detskimi-14125659?option=42199384",
+  },
+  {
+    title: `${ICON} Набор книжек с наклейками для детей 2`,
+    link: "https://www.wildberries.by/product/nabor-knizhek-s-naklejkami-dlya-detej-14125661?option=42199386",
+  },
+  {
+    title: `${ICON} Океанариум`,
+    link: "https://www.wildberries.by/product/okeanarium-9969852?option=32653917",
+  },
+  {
+    title: `${ICON} Пальчиковые краски`,
+    link: "https://www.wildberries.by/product/palchikovye-kraski-dlya-malyshej-s-raskraskami-i-shtampami-156663853?option=261313018",
   },
   { title: `${ICON} Сертификат в ZARA` },
   { title: `${ICON} Сертификат в ДетМир` },
