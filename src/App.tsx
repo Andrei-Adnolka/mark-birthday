d71e227fc0a30a35ace127e6816657835ee7a354
Andrei Adnolka <andrei.adnolka@gmail.com>
2025-08-21 21:32:46 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import JSConfetti from "js-confetti";
 import React, { useCallback } from "react";
 
-import MapUI from "./components/map";
-import Gifts from "./components/gifts";
-import SliderUI from "./components/slider";
-
 import Hero from "./components/new/hero";
 import Bento from "./components/new/bento-wrapper";
 
 import "./App.scss";
-import { TimerUI } from "./components/timer";
 
 function App() {
   const [confetti] = React.useState(() => new JSConfetti());
